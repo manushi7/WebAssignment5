c70b20185ab78ef733fcbc1bd8a7fd1c5aad2e93
Loise College <144301256+loisem444@users.noreply.github.com>
2024-03-27 23:57:28 -0400

    Implement weather widget

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,25 +1,18 @@
 import React from 'react';
 
+import WeatherWidget from '../components/WeatherWidget'
+
 const DashboardPage = () => {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-      <div>
-        <h3>User Profile Information</h3>
-        <ul>
-          <li>Email: manushi@gmail.com</li>
-          <li>Name: Manushi Khadka</li>
-          <li>Role: Administrator</li>
-          <li>Joined Date: January 1, 2023</li>
-          <li>Last Login: March 15, 2023</li>
-          <li>Status: Active</li>
-        </ul>
-      </div>
-      <div>
-        <h3>Edit your information</h3>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+          <h2>Dashboard</h2>
+          <div>
+          </div>
+          <div>
+            <WeatherWidget />
+          </div>
+        </div>
+    );
 };
 
 export default DashboardPage;
